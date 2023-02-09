be1413324ba59b1a860a4e6be7d1e2cd4fd73947
Robin-Ferm <70013388+Robin-Ferm@users.noreply.github.com>
2023-02-09 13:19:00 +0100

    added razor pages checkout just to see differences and similarities

--- BethanysPieShop/Program.cs
+++ BethanysPieShop/Program.cs
@@ -12,6 +12,7 @@ builder.Services.AddSession();
 builder.Services.AddHttpContextAccessor();
 
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 builder.Services.AddDbContext<BethanysPieShopDbContext>(option =>
 {
     option.UseSqlServer(
@@ -29,5 +30,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.MapDefaultControllerRoute();
+app.MapRazorPages();
 DbInitializer.Seed(app);
 app.Run();
